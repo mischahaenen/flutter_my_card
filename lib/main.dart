2881a9684ea790615dc2127e1d27e3bd2534c0c9
Mischa Haenen <mischa.haenen@yahoo.de>
2019-11-03 12:19:40 +0100

    add: profile avatar

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('lib/assets/images/Mischa2_2.jpg'),
+                backgroundColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
